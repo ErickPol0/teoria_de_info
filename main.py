211dbf579f873bed8f891ac3b89a1b3f5a660337
Erick <60324591+ErickPol0@users.noreply.github.com>
2023-10-31 17:22:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ from esquema import *
 from huffman import CodificacionHuffman
 from shannon_fano import ShannonFano
 
-texto_original = fuente()
-
 metodo = int(input('Decida el metodo de codificacion:\n1 - Hoffman\n2 - Shannon\n'))
 if metodo == 1:
     codificador = CodificacionHuffman()
-    tabla_codigos = codificador.tabla(texto_original)
-    texto_codificado = transmisor(texto_original,tabla_codigos)
-    texto_codificado= canal(texto_codificado)
-    texto_decodificado = receptor(texto_codificado,tabla_codigos)
-    destino(texto_decodificado)
 
 else:
     codificador = ShannonFano()
-    tabla_codigos = codificador.tabla(texto_original)
-    texto_codificado = transmisor(texto_original,tabla_codigos)
-    texto_codificado = canal(texto_codificado)
-    texto_decodificado = receptor(texto_codificado,tabla_codigos)
-    destino(texto_decodificado)
+
+
+longitud_paquete = 4  # Puedes ajustar este valor según tus necesidades
+probabilidad_error = 0.3  # Puedes ajustar la probabilidad de error
+texto_original = fuente()
+tabla_codigos = codificador.tabla(texto_original)
+paquetes = transmisor(texto_original, tabla_codigos, longitud_paquete)
+paquetes_recibidos, paquetes_perdidos = canal(paquetes, probabilidad_error)
+
+
+#texto_codificado= canal(texto_codificado)
+#texto_decodificado = receptor(texto_codificado,tabla_codigos)
+#destino(texto_decodificado)
 
 
     
