a938b3bbbd590419a2c8d0c7683d16f0243aa2d4
Erick <60324591+ErickPol0@users.noreply.github.com>
2023-11-20 22:42:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,17 @@ from shannon_fano import ShannonFano
 metodo = int(input('Decida el metodo de codificacion:\n1 - Hoffman\n2 - Shannon\n'))
 if metodo == 1:
     codificador = CodificacionHuffman()
-
 else:
     codificador = ShannonFano()
 
-
-longitud_paquete = 4  # Puedes ajustar este valor según tus necesidades
-probabilidad_error = 0.3  # Puedes ajustar la probabilidad de error
+longitud_paquete = 4
+probabilidad_error = 0.4
 texto_original = fuente()
 tabla_codigos = codificador.tabla(texto_original)
 paquetes = transmisor(texto_original, tabla_codigos, longitud_paquete)
-paquetes_recibidos, paquetes_perdidos = canal(paquetes, probabilidad_error)
-
-
-#texto_codificado= canal(texto_codificado)
-#texto_decodificado = receptor(texto_codificado,tabla_codigos)
-#destino(texto_decodificado)
-#holaaa
-
+paquetes_recuperados = adaptacion_modulativa(paquetes, probabilidad_error)
+texto_decodificado = receptor(paquetes_recuperados,tabla_codigos)
+destino(texto_decodificado)
 
     
 
